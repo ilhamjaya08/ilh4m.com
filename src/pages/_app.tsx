0ee53481035adeae4178aa8def93f74483a7a7be
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-12-03 08:36:18 +0700

    feat: DM Sans font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { DM_Sans } from 'next/font/google';
+
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={dmSans.className}>
       <Header />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </main>
   );
 }
