4f68f24ca0898da270fe08a49d5fcc783b34a045
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-12-03 08:47:25 +0700

    feat: implement seo meta on app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,56 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class Document extends NextScript {
+  static async getInitialProps(ctx: DocumentContext): Promise<{
+    styles: JSX.Element;
+    html: string;
+    head?: (JSX.Element | null)[] | undefined;
+  }> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="Muhammad Ilham Jaya's portfolio website. I am a passionate software developer with expertise in web development, creating innovative solutions and delivering high-quality applications." />
+          <meta name="keywords" content="ilham, ilhamjaya08, portfolio, nextjs, seo, dev, developer, programmer, tailwind, muhammad ilham jaya" />
+          <meta property="og:title" content="Ilham.live" />
+          <meta property="og:description" content="Muhammad Ilham Jaya's portfolio website. I am a passionate software developer with expertise in web development, creating innovative solutions and delivering high-quality applications." />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://ilham.live" />
+          <meta property="og:image" content="https://ilham.live/og.png" />
+          <link rel="canonical" href="https://ilham.live" />
+        </Head>
+        <body className="antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
